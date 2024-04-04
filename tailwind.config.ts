b85232b583e9c6f6b2aedce0c309e151d7c773a7
Elle Fiorentino-Lange <sage.fio.lange@gmail.com>
2024-04-03 21:00:46 -0400

    colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-import colors from "./utils/constants/colors";
 
 const config: Config = {
   content: [
@@ -7,15 +6,38 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  // blocklist: ["invert"],
   theme: {
     extend: {
-      colors,
+      colors: {
+        light: "rgb(var(--light) / <alpha-value>)",
+        dark: "rgb(var(--dark) / <alpha-value>)",
+        // ===========================================
+        alabaster: "rgb(var(--alabaster) / <alpha-value>)",
+        sage: "rgb(var(--sage) / <alpha-value>)",
+        cave: "rgb(var(--cave) / <alpha-value>)",
+        // ===========================================
+        red: "rgb(var(--red) / <alpha-value>)",
+        // ===========================================
+        fg: "rgb(var(--fg) / <alpha-value>)",
+        bg: "rgb(var(--bg) / <alpha-value>)",
+        // ===========================================
+        accent: {
+          fg: "rgb(var(--accent-fg) / <alpha-value>)",
+          mid: "rgb(var(--accent-mid) / <alpha-value>)",
+          bg: "rgb(var(--accent-bg) / <alpha-value>)",
+        },
+        // rose: "rgb(var(--rose) / <alpha-value>)",
+        // eggplant: "rgb(var(--eggplant) / <alpha-value>)",
+      },
       fontFamily: {
         cormorant: ["var(--cormorant)"],
       },
       animation: {
         "fade-in": "fade-in 1s ease-in-out",
+        "fade-in-quick": "fade-in 0.25s ease-in-out",
         "fade-out": "fade-out 1s ease-in-out",
+        "fade-out-quick": "fade-out 0.25s ease-in-out",
       },
       keyframes: {
         "fade-in": {
--- utils/constants/colors.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  light: "#FBF9F4",
-  dark: "#20222E",
-  red: "#DD2C4C",
-  eggplant: "#502536",
-  salmon: "#EC93A0",
-};
