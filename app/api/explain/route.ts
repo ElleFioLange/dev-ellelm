937abf90ce6067043dbb0ad59875831a8e7366df
Elle Fiorentino-Lange <sage.fio.lange@gmail.com>
2024-04-04 17:18:02 -0400

    use Claude

--- app/api/explain/route.ts
+++ app/api/explain/route.ts
@@ -1,6 +1,13 @@
 import OpenAI from "openai";
+import { AnthropicStream, OpenAIStream, StreamingTextResponse } from "ai";
 import keywords from "@/utils/keywords";
-import { OpenAIStream, StreamingTextResponse } from "ai";
+import Anthropic from "@anthropic-ai/sdk";
+
+export const runtime = "edge";
+
+const anthropic = new Anthropic({
+  apiKey: process.env.ANTHROPIC_API_KEY,
+});
 
 export type Req = {
   selected: string[];
@@ -14,20 +21,34 @@ export async function POST(req: Request) {
       (item) => keywords[item.toLowerCase() as "developer"]
     );
 
-    const openai = new OpenAI();
+    // const openai = new OpenAI();
 
-    const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      stream: true,
+    // const response = await openai.chat.completions.create({
+    //   model: "gpt-3.5-turbo",
+    //   stream: true,
+    //   messages: [
+    //     {
+    //       role: "user",
+    //       content: "Give me a 500 word report on the phoenician empire.",
+    //     },
+    //   ],
+    // });
+
+    // const stream = OpenAIStream(response);
+
+    const response = await anthropic.messages.create({
       messages: [
         {
           role: "user",
           content: "Give me a 500 word report on the phoenician empire.",
         },
       ],
+      model: "claude-3-haiku-20240307",
+      stream: true,
+      max_tokens: 1024,
     });
 
-    const stream = OpenAIStream(response);
+    const stream = AnthropicStream(response);
 
     return new StreamingTextResponse(stream, { status: 200 });
   } catch (e) {
